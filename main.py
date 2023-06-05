2a88dd08edb90b5dcac63381414f65b1737886e5
jesseb_rev <jessebenjamincpa@gmail.com>
2023-06-05 14:01:52 -0400

    Movement
    Updated Keyboard input and added boundaries to player movement

--- main.py
+++ main.py
@@ -17,9 +17,11 @@ pygame.display.set_icon(icon)
 playerImg = pygame.image.load('imgs/space-invaders.png')
 playerX = 370 
 playerY = 480
+playerX_change = 0
     
-def player():
-    screen.blit(playerImg, (playerX, playerY)) # draw (what,where)
+def player(x, y):
+    # screen.blit(playerImg, (playerX, playerY))
+    screen.blit(playerImg, (x, y)) # draw (what,where)
 
 # keep our game running
 # Game Loop
@@ -29,12 +31,31 @@ while running:
     # RGB - Red, Green, Blue
     # this is drawn first
     screen.fill((0,0,0)) # by default this doesn't work - we need to update it
-
+    #playerX += .01
             
     for event in pygame.event.get():
         if event.type == pygame.QUIT: # if the X button has been pressed, i.e. close
             running = False
-    # this will appear constantly in the game
+        # this will appear constantly in the game
 
-    player()
+        # if keystroke is pressed, check whether its right or left
+        if event.type == pygame.KEYDOWN:
+            #print("A keystroke is pressed")
+            if event.key == pygame.K_LEFT:
+                #print("Left arrow is pressed")
+                playerX_change = - 0.1
+            if event.key == pygame.K_RIGHT:
+                #print("Right arrow is pressed")
+                playerX_change = + 0.1
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT: 
+                #print("Keystoke has been released")
+                playerX_change = 0.0
+    playerX += playerX_change
+
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >= 736: # 800-64
+        playerX = 736
+    player(playerX, playerY)
     pygame.display.update() # need to dispaly stuff
